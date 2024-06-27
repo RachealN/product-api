4574011e0406bbb3a8ac39f4a545a3fb64c27c6f
RachealN <rechealmatsikoit@gmail.com>
2024-06-27 13:17:19 +0300

    Set up PHPStan and refactor controllers/repositories to use services

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -2,23 +2,24 @@
 
 namespace App\Controller;
 
-use App\Repository\ProductRepository;
+use App\Service\ProductService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
-use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Routing\Annotation\Route;
 
 class ProductsController extends AbstractController
 {
-    private $productRepository;
+    private $productService;
 
-    public function __construct(ProductRepository $productRepository)
+    public function __construct(ProductService $productService)
     {
-        $this->productRepository = $productRepository;
+        $this->productService = $productService;
     }
+
     #[Route('/products', name: 'products')]
-    public function all(): JsonResponse
+    public function index(): JsonResponse
     {
-        $products = $this->productRepository->findAll();
+        $products = $this->productService->getAllProducts();
         return $this->json($products);
     }
 }
