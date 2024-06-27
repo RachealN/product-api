ed0b6e34e25aeae869ac30a12459ec3cd0212ab4
RachealN <rechealmatsikoit@gmail.com>
2024-06-27 15:37:34 +0300

    test for fetching all products, rename src/Services to src/Service

--- tests/ProductTest.php
+++ tests/ProductTest.php
@@ -3,6 +3,8 @@
 namespace App\Tests;
 
 use App\Kernel;
+use App\Entity\Product;
+use PHPUnit\Framework\Attributes\Test;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
 
 class ProductTest extends KernelTestCase
@@ -18,8 +20,33 @@ class ProductTest extends KernelTestCase
         $this->entityManager = $kernel->getContainer()->get('doctrine')->getManager();
     }
 
-    public function testProducts()
+    protected function tearDown(): void
     {
-        $this->assertTrue(true);
+        parent::tearDown();
+
+        $this->entityManager->close();
+        $this->entityManager = null;
+    }
+
+    /**
+     * @test
+     */
+    public function test_can_get_all_products()
+    {
+        $product = new Product();
+        $product->setName('Car');
+        $product->setPrice('5000');
+        $product->setStockQuantity(1);
+        $product->setDescription('Lorem ipsum dolor sit amet consectetur adipisicing elit');
+
+        $this->entityManager->persist($product);
+        $this->entityManager->flush();
+
+        //product repository
+        $productRepository = $this->entityManager->getRepository(Product::class);
+
+        $stockProduct = $productRepository->findOneBy(['name' => 'Car']);
+        $this->assertEquals('Car', $stockProduct->getName());
+        $this->assertEquals('5000', $stockProduct->getPrice());
     } 
 }
